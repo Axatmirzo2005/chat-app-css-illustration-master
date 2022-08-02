9cf8e5614c05ac696d11cbec7ddff3a110626239
Axatmirzo <99807404+Axatmirzo2005@users.noreply.github.com>
2022-08-02 22:59:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+// function track() {
+      
+//       var r = prompt("Enter your parol", "148");
+//       if (r == 123) {
+//             alert("parol is correct" + "Welcome to my dier");
+//             d.t2.value="Parol is correct";
+//             d.style.display="flex";
+
+//             d1.t3.value=d1.t3.value;
+//             d1.style.display="flex";
+//       } else {
+//             alert("parol is wrong");
+//             dl.t.value="Parol is wrong";
+//             dl.style.display="flex";
+
+//             d2.t4.value=d2.t4.value;
+//             d2.style.display="flex";
+//       }
+// }
+
+function track() {
+      dls.t5.value=f.t1.value;
+      dls.style.display="flex";
+
+      
+      if (dls.t5.value=="Hello", "salom", "Salom") {
+            dlsans.style.display="flex";
+      } else {
+            dlsans.style.display="none";
+      }
+};
+
+function bos1() {
+      var cir = document.getElementsByClassName("ciron");
+
+      cir[0].style.display="flex";
+      cir[0].style.boxShadow="0 0 20px 5px red";
+      confirm("Success!");
+}
+
+function bos2() {
+      var cir = document.getElementsByClassName("ciron");
+
+      cir[1].style.display="flex";
+      cir[1].style.boxShadow="0 0 20px 5px red";
+      confirm("Success!");
+}
